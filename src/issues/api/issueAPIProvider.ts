012ff60f9d64983d001d65825fb22bae345795a6
Dmitrii <dimasik007@rambler.ru>
2024-11-13 22:02:34 +0300

    Описал APIProvider

--- src/issues/api/issueAPIProvider.ts
+++ src/issues/api/issueAPIProvider.ts
@@ -3,7 +3,7 @@ import { GitAPIProvider } from "../../common/api/GitAPIProvider";
 import { CreateLabelRequest } from "./IssueAPIDataProvider";
 
 class IssueAPIProvider extends GitAPIProvider {
-    public async CreateLabel<T>(owner: string, repository: string, data: CreateLabelRequest): Promise<AxiosResponse<T>> {
+    public async createLabel<T>(owner: string, repository: string, data: CreateLabelRequest): Promise<AxiosResponse<T>> {
         const config: AxiosRequestConfig = this.configureRequest(
             `/repos/${owner} / ${repository} / labels`,
             'POST',
@@ -12,15 +12,22 @@ class IssueAPIProvider extends GitAPIProvider {
         return this.sendRequest(config)
     }
 
-    public async getCreateIssue<T>(): Promise<AxiosResponse<T>> {
+    public async createIssue<T>(owner: string, repository: string, data: CreateLabelRequest): Promise<AxiosResponse<T>> {
         const config: AxiosRequestConfig = this.configureRequest(
-            '/repos/{owner}/{repo}/issues',
-            'POST'
+            `/repos/${owner}/${repository}/issues`,
+            'POST',
+            JSON.stringify(data)
         )
         return this.sendRequest(config)
     }
 
-    public async getDeleteLabel<T>(): Promise
+    public async deleteLabel<T>(owner: string, repository: string, name: string, data: CreateLabelRequest): Promise<AxiosResponse<T>> {
+        const config: AxiosRequestConfig = this.configureRequest(
+            `/repos/${owner}/${repository}/labels/${name}`,
+            'DELETE'
+        )
+        return this.sendRequest(config)
+    }
 
 }
 

925bf9655e3f08af6b784e22e578c0dafa154f37
dmitriy.potapov <dmitriy.potapov@ispring.com>
2024-11-19 09:26:17 +0300

    Добавил API тесты

--- src/issues/api/test/createIssue.test.ts
+++ src/issues/api/test/createIssue.test.ts
@@ -12,11 +12,11 @@ const REPOSITORY = 'autotest-ips'
 describe('Create issue', () => {
     const issue: IssueModel = createIssueModel()
 
-    it('Issue with label should be created', async () => {
+    it('Issue with label should be created, code is OK', async () => {
         const data: CreateIssueRequest = {
             title: issue.title,
             body: issue.description,
-            labels: issue.label.name //брать из модели
+            labels: labelData.name[0] //брать из модели
         }
         const issueAPIProvider = new IssueAPIProvider({
             isSuccesfulResponse: false
@@ -28,6 +28,35 @@ describe('Create issue', () => {
         expect(response.status).toEqual(201)
         expect(response.data.title).toEqual(data.title)
         expect(response.data.body).toEqual(data.body)
+        expect(response.request.config).toEqual('GET')
         // expect(response.data.labels).toEqual(data.labels) //проверить label, body
+    })
+
+    it('Issue should not be created, code is Gone', async () => {
+        const data: CreateIssueRequest = {
+            title: issue.title,
+            body: issue.description,
+            labels: labelData.name[2] //брать из модели
+        }
+        const issueAPIProvider = new IssueAPIProvider({
+            isSuccesfulResponse: false
+        })
+        const response: AxiosResponse<CreateIssueResponse> = await issueAPIProvider.createIssue(OWNER, REPOSITORY, data)
+
+        expect(response.status).toEqual(410)
+    })
+
+    it('Issue should not be created, code is Bad Request', async () => {
+        const data: CreateIssueRequest = {
+            title: '',
+            body: issue.description,
+            labels: ''
+        }
+        const issueAPIProvider = new IssueAPIProvider({
+            isSuccesfulResponse: false
+        })
+        const response: AxiosResponse<CreateIssueResponse> = await issueAPIProvider.createIssue(OWNER, REPOSITORY, data)
+
+        expect(response.status).toEqual(400)
     })
 })
